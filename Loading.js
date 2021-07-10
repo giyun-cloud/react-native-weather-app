fba07775ca38460fa47e776a841bce4c9ccd123e
giyun-cloud <nangiyoon@gmail.com>
2021-07-11 06:59:52 +0900

    1.2 Getting the Location

--- Loading.js
+++ Loading.js
@@ -13,7 +13,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "flex-end",
-    backgroundColor: "#dfe6e9",
+    backgroundColor: "#ffeaa7",
     paddingHorizontal: 30,
     paddingVertical: 100,
   },
