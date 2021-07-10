4b5bf303a78fa4c478a07ba4892c88f83cd245c3
giyun-cloud <nangiyoon@gmail.com>
2021-07-10 16:30:49 +0900

    1.0 Layouts with Flexbox in React Native

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.thirdBox}></View>
+      <View style={styles.secondBox}></View>
+      <View style={styles.firstBox}></View>
     </View>
   );
 }
@@ -14,8 +15,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#2ecc71",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#8e44ad",
+  },
+  firstBox: {
+    flex: 1,
+    backgroundColor: "#2c3e50",
+  },
+  secondBox: {
+    flex: 1,
+    backgroundColor: "#95a5a6",
+  },
+  thirdBox: {
+    flex: 1,
+    backgroundColor: "#7f8c8d",
   },
 });
