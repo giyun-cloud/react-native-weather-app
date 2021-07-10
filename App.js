54b328edd83b9f2ed5896bfb1ad251c428c3c08e
giyun-cloud <nangiyoon@gmail.com>
2021-07-10 21:28:22 +0900

    1.1 Loading Screen

--- App.js
+++ App.js
@@ -1,36 +1,6 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import Loading from "./Loading";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.thirdBox}></View>
-      <View style={styles.secondBox}></View>
-      <View style={styles.firstBox}></View>
-    </View>
-  );
+  return <Loading />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#2ecc71",
-    justifyContent: "center",
-  },
-  text: {
-    color: "#8e44ad",
-  },
-  firstBox: {
-    flex: 1,
-    backgroundColor: "#2c3e50",
-  },
-  secondBox: {
-    flex: 1,
-    backgroundColor: "#95a5a6",
-  },
-  thirdBox: {
-    flex: 1,
-    backgroundColor: "#7f8c8d",
-  },
-});
---
+++ Loading.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Loading() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Getting the fucking Weather</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-end",
+    backgroundColor: "#dfe6e9",
+    paddingHorizontal: 30,
+    paddingVertical: 100,
+  },
+  text: {
+    fontSize: 30,
+    color: "#2d3436",
+  },
+});
