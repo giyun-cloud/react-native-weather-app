0431722926e49207255ea3ceb1c4ed25c38b36a2
giyun-cloud <nangiyoon@gmail.com>
2021-07-12 09:38:02 +0900

    2.3 Background Gradient

--- Weather.js
+++ Weather.js
@@ -1,17 +1,90 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, StatusBar } from "react-native";
 import PropTypes from "prop-types";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
+
+const weatherOptions = {
+  Thunderstorm: {
+    icons: "weather-lightning",
+    gradient: ["#FFE000", "#799F0C"],
+  },
+  Drizzle: {
+    icons: "weather-rainy",
+    gradient: ["#2980B9", "#6DD5FA", "#FFFFFF"],
+  },
+  Rain: {
+    icons: "weather-rainy",
+    gradient: ["#00d2ff", "#3a7bd5"],
+  },
+  Snow: {
+    icons: "weather-snowy",
+    gradient: ["#E6DADA", "#274046"],
+  },
+  Mist: {
+    icons: "grain",
+    gradient: ["#36D1DC", "#5B86E5"],
+  },
+  Smoke: {
+    icons: "weather-cloudy-alert",
+    gradient: ["#636363", "#a2ab58"],
+  },
+  Haze: {
+    icons: "weather-fog",
+    gradient: ["#36D1DC", "#5B86E5"],
+  },
+  Dust: {
+    icons: "weather-cloudy-alert",
+    gradient: ["#636363", "#a2ab58"],
+  },
+  Fog: {
+    icons: "weather-fog",
+    gradient: ["#36D1DC", "#5B86E5"],
+  },
+  Sand: {
+    icons: "weather-cloudy-alert",
+    gradient: ["#636363", "#a2ab58"],
+  },
+  Ash: {
+    icons: "weather-fog",
+    gradient: ["#636363", "#a2ab58"],
+  },
+  Squall: {
+    icons: "weather-windy",
+    gradient: ["#1f4037", "#99f2c8"],
+  },
+  Tornado: {
+    icons: "weather-tornado",
+    gradient: ["#44A08D", "#093637"],
+  },
+  Clear: {
+    icons: "weather-sunny",
+    gradient: ["#43C6AC", "#F8FFAE"],
+  },
+  Clouds: {
+    icons: "weather-cloudy",
+    gradient: ["#B2FEFA", "#0ED2F7"],
+  },
+};
 
 function Weather({ temp, weather }) {
   return (
-    <View style={styles.container}>
+    <LinearGradient
+      // Background Linear Gradient
+      colors={weatherOptions[weather].gradient}
+      style={styles.container}
+    >
+      <StatusBar barStyle="light-content" />
       <View style={styles.halfContainer}>
-        <MaterialCommunityIcons size={96} name="weather-rainy" />
+        <MaterialCommunityIcons
+          size={96}
+          name={weatherOptions[weather].icons}
+          color="#fff"
+        />
         <Text style={styles.temp}>{temp}°</Text>
       </View>
       <View style={styles.halfContainer}></View>
-    </View>
+    </LinearGradient>
   );
 }
 
@@ -23,6 +96,7 @@ const styles = StyleSheet.create({
   },
   temp: {
     fontSize: 42,
+    color: "#fff",
   },
   halfContainer: {
     flex: 1,
